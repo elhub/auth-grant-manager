432d7d5cc7126b0de25d046a28dbf608ae424b5a
Michael Akinde <michael.akinde@statnett.no>
2025-02-18 14:44:35 +0100

    Fix gradle wrapper URI

--- src/main/kotlin/no/elhub/devxp/config/Security.kt
+++ src/main/kotlin/no/elhub/devxp/config/Security.kt
@@ -3,4 +3,6 @@ package no.elhub.devxp.config
 import io.ktor.server.application.Application
 
 fun Application.configureSecurity() {
+    // Implement MaskinPorten - energy-data
+    // Implement OIDC for MinSide
 }
