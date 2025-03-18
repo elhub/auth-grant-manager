15f864ee36b31aae597002e058d9fad4a1daf5e8
Michael Akinde <michael.akinde@statnett.no>
2025-03-18 13:34:40 +0100

    Add initial logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation(libs.bundles.ktor.koin)
     ksp(libs.di.koin.ksp.compiler)
     // Logging
-    implementation(libs.logging.logback.classic)
+    implementation(libs.bundles.logging)
     // Monitoring
     implementation(libs.bundles.ktor.monitoring)
     // Database
--- src/main/kotlin/no/elhub/devxp/config/Logging.kt
+++ src/main/kotlin/no/elhub/devxp/config/Logging.kt
@@ -1,9 +1,9 @@
 package no.elhub.devxp.config
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.calllogging.CallLogging
 
 fun Application.configureLogging() {
-    // slf4jLogger() is a function that configures the Koin logger to use the SLF4J logger.
-    // Logback is a logging framework that is a successor to the popular log4j project.
-    // LogFMT is a structured logging library that allows you to log messages in a structured way.
+    install(CallLogging)
 }
