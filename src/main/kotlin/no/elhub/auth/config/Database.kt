8f06d7c42b448aee92ebb4bdb2afc140394855b0
Michael Akinde <michael.akinde@statnett.no>
2025-03-31 13:10:54 +0200

    Fix hard-coded variables; ensure configuration of secrets through environment
    ## 📝 Description
    
    This ensures that configuration of database username and password happens through the environment.
    
    Test credentials are still passed in automatically.
    
    ## 📋 Checklist
    
    * ✅ Lint checks passed on local machine.
    * ✅ Unit tests passed on local machine.
    * ✅ Documentation Updates: README

--- src/main/kotlin/no/elhub/auth/config/Database.kt
+++ src/main/kotlin/no/elhub/auth/config/Database.kt
@@ -7,11 +7,11 @@ import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabase(): HikariDataSource {
     val config = HikariConfig().apply {
-        jdbcUrl = "jdbc:postgresql://localhost:5432/postgres"
-        driverClassName = "org.postgresql.Driver"
-        username = "postgres"
-        password = "postgres"
-        maximumPoolSize = 3
+        jdbcUrl = environment.config.propertyOrNull("ktor.database.url")?.getString() ?: "jdbc:postgresql://localhost:5432/auth"
+        driverClassName = environment.config.propertyOrNull("ktor.database.driverClass")?.getString() ?: "org.postgresql.Driver"
+        username = environment.config.propertyOrNull("ktor.database.username")?.getString()
+        password = environment.config.propertyOrNull("ktor.database.password")?.getString()
+        maximumPoolSize = environment.config.propertyOrNull("ktor.database.hikari.maximumPoolSize")?.getString()?.toInt() ?: 3
     }
     val dataSource = HikariDataSource(config)
     Database.connect(dataSource)
