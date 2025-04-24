0f38f2cd2c4a3dfa5ebb59aa4dc5f11197596f52
Michael Akinde <michael.akinde@statnett.no>
2025-04-24 12:06:52 +0200

    Fix the TeamCity build config
    ## 📋 Checklist
    
    * ✅ Lint checks passed on local machine.
    * ✅ Unit tests passed on local machine.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,21 +1,45 @@
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
 import no.elhub.devxp.build.configuration.pipeline.constants.Group
+import no.elhub.devxp.build.configuration.pipeline.constants.KubeCluster
+import no.elhub.devxp.build.configuration.pipeline.extensions.triggerOnVcsChange
+import no.elhub.devxp.build.configuration.pipeline.jobs.gitOps
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleJib
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
-val imageRepo = "auth/auth-grant-manager" // TODO: Change this in terraform
+val imageRepo = "auth/auth-grant-manager"
 val gitOpsRepo = "https://github.com/elhub/auth"
 
 elhubProject(group = Group.AUTH, name = "auth-grant-manager") {
     pipeline {
         sequential {
-            gradleVerify {
-            }
+            gradleVerify()
 
             gradleJib {
                 registrySettings = {
                     repository = imageRepo
                 }
+            }
+
+            parallel {
+                gitOps {
+                    cluster = KubeCluster.TEST9
+                    gitOpsRepository = gitOpsRepo
+                }.triggerOnVcsChange()
+
+                gitOps {
+                    cluster = KubeCluster.TEST11
+                    gitOpsRepository = gitOpsRepo
+                }.triggerOnVcsChange()
+
+                gitOps {
+                    cluster = KubeCluster.TEST13
+                    gitOpsRepository = gitOpsRepo
+                }.triggerOnVcsChange()
+
+                gitOps {
+                    cluster = KubeCluster.TEST14
+                    gitOpsRepository = gitOpsRepo
+                }.triggerOnVcsChange()
             }
         }
     }
