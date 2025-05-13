534e5ddcc919689677320c4c18a3d99fa3c21e90
Michael Akinde <michael.akinde@statnett.no>
2025-05-13 18:28:25 +0200

    Add architecture documentation and minor schema fixes

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,8 +1,8 @@
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
 import no.elhub.devxp.build.configuration.pipeline.constants.Group
+import no.elhub.devxp.build.configuration.pipeline.extensions.triggerOnVcsChange
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleJib
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
-import no.elhub.devxp.build.configuration.pipeline.extensions.triggerOnVcsChange
 
 val imageRepo = "auth/auth-grant-manager" // TODO: Change this in terraform
 val gitOpsRepo = "https://github.com/elhub/auth"
