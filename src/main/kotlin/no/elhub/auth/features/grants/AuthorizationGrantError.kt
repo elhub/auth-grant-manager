ad06b9b81605d08a8b853818928ace40e85ec938
Lise May Le <lise.le@statnett.no>
2025-06-11 08:10:37 +0200

    Lint fix for AuthorizationGrantError

--- src/main/kotlin/no/elhub/auth/features/grants/AuthorizationGrantError.kt
+++ src/main/kotlin/no/elhub/auth/features/grants/AuthorizationGrantError.kt
@@ -1,8 +1,11 @@
 package no.elhub.auth.features.grants
 
 sealed class AuthorizationGrantError {
-    data object NotFoundError: AuthorizationGrantError()
+    data object NotFoundError : AuthorizationGrantError()
+
     data object DataBaseError : AuthorizationGrantError()
-    data object InternalServerError: AuthorizationGrantError()
-    data object IllegalArgumentError: AuthorizationGrantError()
+
+    data object InternalServerError : AuthorizationGrantError()
+
+    data object IllegalArgumentError : AuthorizationGrantError()
 }
