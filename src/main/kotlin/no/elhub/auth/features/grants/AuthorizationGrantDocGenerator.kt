050d4df29503b55cb7206a36a53a731d91d4615a
Lise May Le <lise.le@statnett.no>
2025-07-23 12:51:55 +0200

    Add error response to the openapi.yaml file

--- src/main/kotlin/no/elhub/auth/features/grants/AuthorizationGrantDocGenerator.kt
+++ src/main/kotlin/no/elhub/auth/features/grants/AuthorizationGrantDocGenerator.kt
@@ -13,6 +13,7 @@ import no.elhub.auth.openapi.RelationshipData
 import no.elhub.auth.openapi.RelationshipInnerData
 import no.elhub.auth.openapi.TopLevelLinks
 import no.elhub.auth.openapi.TopLevelMeta
+import no.elhub.auth.openapi.ApiErrorResponse
 
 class AuthorizationGrantDocGenerator {
 
@@ -21,18 +22,34 @@ class AuthorizationGrantDocGenerator {
     companion object {
         const val AUTHORIZATION_GRANT_GET_ALL_PATH = AUTHORIZATION_GRANT
         const val AUTHORIZATION_GRANT_GET_ID_PATH = "$AUTHORIZATION_GRANT/{id}"
+        const val AUTHORIZATION_GRANT_GET_SCOPE_PATH = "$AUTHORIZATION_GRANT/{id}/scopes"
     }
 
     @Path(AUTHORIZATION_GRANT_GET_ALL_PATH)
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(
-        summary = "Get all Authorization Grant. ",
+        summary = "Retrieve a list of authorization grants. ",
         responses = [
             ApiResponse(
                 responseCode = "200",
                 description = "Successful retrieve all. ",
                 content = [Content(schema = Schema(implementation = AuthorizationGrantListResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad request. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
             )
         ]
     )
@@ -53,11 +70,70 @@ class AuthorizationGrantDocGenerator {
                 responseCode = "200",
                 description = "Successful retrieve Authorization Grant. ",
                 content = [Content(schema = Schema(implementation = AuthorizationGrantResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad request. Invalid request body. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Not found. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
             )
         ]
     )
-
     fun authorizationGrantDocResponse(): AuthorizationGrantResponse =
+        AuthorizationGrantResponse(
+            data = authorizationGrantObject,
+            links = TopLevelLinks(self = ""),
+            meta = TopLevelMeta(createdAt = "")
+        )
+
+    @Path(AUTHORIZATION_GRANT_GET_SCOPE_PATH)
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary = "Retrieve all of the scopes for the identified authorization grant. ",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Successful retrieve Authorization Grant. ",
+                content = [Content(schema = Schema(implementation = AuthorizationGrantResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad request. Invalid request body. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Not found. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error. ",
+                content = [Content(schema = Schema(implementation = ApiErrorResponse::class))]
+            )
+        ]
+    )
+    fun authorizationGrantScopeDocResponse(): AuthorizationGrantResponse =
         AuthorizationGrantResponse(
             data = authorizationGrantObject,
             links = TopLevelLinks(self = ""),
