eebf7b4950eb86f3f1fc1728879596c08210f39a
Lise May Le <lise.le@statnett.no>
2025-08-01 10:43:54 +0200

    Remove unnecessary route("") wrapper

--- src/main/kotlin/no/elhub/auth/features/requests/AuthorizationRequestRoute.kt
+++ src/main/kotlin/no/elhub/auth/features/requests/AuthorizationRequestRoute.kt
@@ -9,7 +9,6 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
-import io.ktor.server.routing.route
 import io.ktor.server.util.url
 import no.elhub.auth.config.ID
 import no.elhub.auth.features.errors.ApiError
@@ -18,118 +17,116 @@ import no.elhub.auth.features.utils.validateId
 import java.util.UUID
 
 fun Route.requests(requestHandler: AuthorizationRequestHandler) {
-    route("") {
-        get {
-            requestHandler.getAllRequests().fold(
-                ifLeft = { authRequestProblem ->
-                    when (authRequestProblem) {
-                        is AuthorizationRequestProblem.DataBaseError,
-                        is AuthorizationRequestProblem.UnexpectedError ->
-                            call.respond(
-                                HttpStatusCode.InternalServerError,
-                                ApiErrorJson.from(
-                                    ApiError.InternalServerError(detail = "Unexpected error occurred during fetch authorization requests"),
-                                    call.url(),
-                                ),
-                            )
-                        AuthorizationRequestProblem.NotFoundError -> error("NotFoundError should never be returned for findAll()")
-                    }
-                },
-                ifRight = { requests ->
-                    call.respond(
-                        HttpStatusCode.OK,
-                        message = requests.toGetAuthorizationRequestsResponse()
-                    )
-                }
-            )
-        }
-
-        get("/{$ID}") {
-            val id: UUID =
-                validateId(call.parameters[ID]).getOrElse { error ->
-                    call.respond(HttpStatusCode.fromValue(error.status), ApiErrorJson.from(error, call.url()))
-                    return@get
-                }
-
-            requestHandler.getRequestById(id).fold(
-                ifLeft = { authRequestProblem ->
-                    when (authRequestProblem) {
-                        is AuthorizationRequestProblem.NotFoundError ->
-                            call.respond(
-                                HttpStatusCode.NotFound,
-                                ApiErrorJson.from(
-                                    ApiError.NotFound(detail = "Authorization request with id=$id not found"),
-                                    call.url(),
-                                ),
-                            )
-
-                        is AuthorizationRequestProblem.DataBaseError, AuthorizationRequestProblem.UnexpectedError ->
-                            call.respond(
-                                HttpStatusCode.InternalServerError,
-                                ApiErrorJson.from(
-                                    ApiError.InternalServerError(detail = "Unexpected error occurred during fetch authorization request with id=$id"),
-                                    call.url(),
-                                ),
-                            )
-                    }
-                },
-                ifRight = { request ->
-                    call.respond(
-                        status = HttpStatusCode.OK,
-                        message = request.toGetAuthorizationRequestResponse()
-                    )
-                },
-            )
-        }
-
-        post {
-            val payload = try {
-                call.receive<PostAuthorizationRequestPayload>()
-            } catch (badRequestException: BadRequestException) {
-                val cause = badRequestException.cause
-                when (cause) {
-                    is JsonConvertException -> {
+    get {
+        requestHandler.getAllRequests().fold(
+            ifLeft = { authRequestProblem ->
+                when (authRequestProblem) {
+                    is AuthorizationRequestProblem.DataBaseError,
+                    is AuthorizationRequestProblem.UnexpectedError ->
                         call.respond(
-                            HttpStatusCode.BadRequest,
+                            HttpStatusCode.InternalServerError,
                             ApiErrorJson.from(
-                                ApiError.BadRequest(detail = "Authorization request contains extra, unknown, or missing fields."),
+                                ApiError.InternalServerError(detail = "Unexpected error occurred during fetch authorization requests"),
                                 call.url(),
-                            )
+                            ),
                         )
-                    }
+                    AuthorizationRequestProblem.NotFoundError -> error("NotFoundError should never be returned for findAll()")
                 }
-                return@post
+            },
+            ifRight = { requests ->
+                call.respond(
+                    HttpStatusCode.OK,
+                    message = requests.toGetAuthorizationRequestsResponse()
+                )
+            }
+        )
+    }
+
+    get("/{$ID}") {
+        val id: UUID =
+            validateId(call.parameters[ID]).getOrElse { error ->
+                call.respond(HttpStatusCode.fromValue(error.status), ApiErrorJson.from(error, call.url()))
+                return@get
             }
 
-            requestHandler.postRequest(payload).fold(
-                ifLeft = { authRequestProblem ->
-                    when (authRequestProblem) {
-                        is AuthorizationRequestProblem.NotFoundError ->
-                            call.respond(
-                                HttpStatusCode.NotFound,
-                                ApiErrorJson.from(
-                                    ApiError.NotFound(detail = "Authorization request was not inserted correctly into the database. "),
-                                    call.url(),
-                                ),
-                            )
+        requestHandler.getRequestById(id).fold(
+            ifLeft = { authRequestProblem ->
+                when (authRequestProblem) {
+                    is AuthorizationRequestProblem.NotFoundError ->
+                        call.respond(
+                            HttpStatusCode.NotFound,
+                            ApiErrorJson.from(
+                                ApiError.NotFound(detail = "Authorization request with id=$id not found"),
+                                call.url(),
+                            ),
+                        )
 
-                        is AuthorizationRequestProblem.DataBaseError, AuthorizationRequestProblem.UnexpectedError ->
-                            call.respond(
-                                HttpStatusCode.InternalServerError,
-                                ApiErrorJson.from(
-                                    ApiError.InternalServerError(detail = "Unexpected error occurred during insert authorization request. "),
-                                    call.url(),
-                                ),
-                            )
-                    }
-                },
-                ifRight = { request ->
+                    is AuthorizationRequestProblem.DataBaseError, AuthorizationRequestProblem.UnexpectedError ->
+                        call.respond(
+                            HttpStatusCode.InternalServerError,
+                            ApiErrorJson.from(
+                                ApiError.InternalServerError(detail = "Unexpected error occurred during fetch authorization request with id=$id"),
+                                call.url(),
+                            ),
+                        )
+                }
+            },
+            ifRight = { request ->
+                call.respond(
+                    status = HttpStatusCode.OK,
+                    message = request.toGetAuthorizationRequestResponse()
+                )
+            },
+        )
+    }
+
+    post {
+        val payload = try {
+            call.receive<PostAuthorizationRequestPayload>()
+        } catch (badRequestException: BadRequestException) {
+            val cause = badRequestException.cause
+            when (cause) {
+                is JsonConvertException -> {
                     call.respond(
-                        status = HttpStatusCode.Created,
-                        message = request.toGetAuthorizationRequestResponse()
+                        HttpStatusCode.BadRequest,
+                        ApiErrorJson.from(
+                            ApiError.BadRequest(detail = "Authorization request contains extra, unknown, or missing fields."),
+                            call.url(),
+                        )
                     )
                 }
-            )
+            }
+            return@post
         }
+
+        requestHandler.postRequest(payload).fold(
+            ifLeft = { authRequestProblem ->
+                when (authRequestProblem) {
+                    is AuthorizationRequestProblem.NotFoundError ->
+                        call.respond(
+                            HttpStatusCode.NotFound,
+                            ApiErrorJson.from(
+                                ApiError.NotFound(detail = "Authorization request was not inserted correctly into the database. "),
+                                call.url(),
+                            ),
+                        )
+
+                    is AuthorizationRequestProblem.DataBaseError, AuthorizationRequestProblem.UnexpectedError ->
+                        call.respond(
+                            HttpStatusCode.InternalServerError,
+                            ApiErrorJson.from(
+                                ApiError.InternalServerError(detail = "Unexpected error occurred during insert authorization request. "),
+                                call.url(),
+                            ),
+                        )
+                }
+            },
+            ifRight = { request ->
+                call.respond(
+                    status = HttpStatusCode.Created,
+                    message = request.toGetAuthorizationRequestResponse()
+                )
+            }
+        )
     }
 }
