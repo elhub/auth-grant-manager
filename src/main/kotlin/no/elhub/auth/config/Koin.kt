665850de3c7c2e16ba55df2e1a0488c996c979aa
Vebjorn Fjeldberg <vebjorn.fjeldberg@statnett.no>
2025-08-05 10:43:25 +0200

    Add HTTP client logging to debug requests against vault transit

--- src/main/kotlin/no/elhub/auth/config/Koin.kt
+++ src/main/kotlin/no/elhub/auth/config/Koin.kt
@@ -4,6 +4,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
@@ -74,6 +76,9 @@ val httpClientModule = module {
             }
             install(ContentNegotiation) {
                 json()
+            }
+            install(Logging) {
+                level = LogLevel.ALL
             }
         }
     }
