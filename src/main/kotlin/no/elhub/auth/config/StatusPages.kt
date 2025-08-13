13f901526f9247a24715b967badd38ea61a5c5d9
Lise May Le <lise.le@statnett.no>
2025-08-13 12:42:09 +0200

    Fix lint issues

--- src/main/kotlin/no/elhub/auth/config/StatusPages.kt
+++ src/main/kotlin/no/elhub/auth/config/StatusPages.kt
@@ -1,15 +1,15 @@
 package no.elhub.auth.config
 
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.response.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respond
 import no.elhub.auth.features.errors.UnexpectedError
 import no.elhub.auth.features.errors.mapErrorToResponse
 import no.elhub.devxp.jsonapi.response.JsonApiErrorCollection
 
 fun Application.configureStatusPages() {
-
     install(StatusPages) {
         exception<Exception> { call, e ->
             val response = mapErrorToResponse(UnexpectedError.UnexpectedFailure(e))
@@ -19,5 +19,4 @@ fun Application.configureStatusPages() {
             )
         }
     }
-
 }
