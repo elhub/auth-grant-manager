b4b10e85c78e3de3b97bbf6b1ece0791828314bf
Lise May Le <lise.le@statnett.no>
2025-10-31 12:33:54 +0100

    Fix indentation

--- db/tables/0016-authorization-document-signatories.sql
+++ db/tables/0016-authorization-document-signatories.sql
@@ -1,7 +1,7 @@
 --changeset elhub:16
 CREATE TABLE auth.authorization_document_signatories
 (
-    authorization_document_id   UUID                     NOT NULL REFERENCES auth.authorization_document (id) ON DELETE CASCADE,
+    authorization_document_id   UUID              NOT NULL REFERENCES auth.authorization_document (id) ON DELETE CASCADE,
     requested_from              UUID              NOT NULL,
     signed_by                   UUID              NOT NULL,
     signed_at                   TIMESTAMP WITH TIME ZONE NOT NULL,
