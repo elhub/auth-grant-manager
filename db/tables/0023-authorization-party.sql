c3c6694c1ab790869060b0b0192a5301850a5316
Lise May Le <lise.le@statnett.no>
2025-11-12 09:53:47 +0100

    Revert firle to version from commit c1e756

--- db/tables/0023-authorization-party.sql
+++ db/tables/0023-authorization-party.sql
@@ -2,7 +2,7 @@
 CREATE TABLE auth.authorization_party
 (
   id                UUID                                   NOT NULL,
-  type              auth.party_type                        NOT NULL,
+  type              auth.elhub_resource                    NOT NULL,
   resource_id       VARCHAR(36)                            NOT NULL,
   created_at        TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
   PRIMARY KEY (id),
