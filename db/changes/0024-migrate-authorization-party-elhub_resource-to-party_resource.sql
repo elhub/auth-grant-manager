c1e7563551af0dfa717550f0e26ec5797e95513b
Lise May Le <lise.le@statnett.no>
2025-11-12 09:36:09 +0100

    The database hasn't been populated yet - remove the changeset file for changing the column type

--- db/changes/0024-migrate-authorization-party-elhub_resource-to-party_resource.sql
+++
@@ -1,4 +0,0 @@
---changeset elhub:24
-ALTER TABLE auth.authorization_party
-ALTER COLUMN type TYPE auth.party_resource
-  USING type::text::auth.party_resource;
--- db/types/0012-party_resource.sql
+++
@@ -1,6 +0,0 @@
---changeset elhub:12
-CREATE TYPE auth.party_resource AS ENUM (
-    'Organization',
-    'OrganizationEntity',
-    'Person'
-)
