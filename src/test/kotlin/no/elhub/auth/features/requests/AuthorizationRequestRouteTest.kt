7b5e5a178a297a674f20730a5ca5ee99e77f37d8
Lise May Le <lise.le@statnett.no>
2025-11-20 10:39:47 +0100

    Remove unused import

--- src/test/kotlin/no/elhub/auth/features/requests/AuthorizationRequestRouteTest.kt
+++ src/test/kotlin/no/elhub/auth/features/requests/AuthorizationRequestRouteTest.kt
@@ -8,7 +8,6 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
-import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
