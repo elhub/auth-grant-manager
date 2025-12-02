f25436b9564117cd6aa5b9c6e66bf01f73bd99a0
Vebjorn Fjeldberg <vebjorn.fjeldberg@statnett.no>
2025-12-02 09:36:46 +0100

    Change patch document pdf to put returning 204

--- src/main/kotlin/no/elhub/auth/features/documents/confirm/Route.kt
+++ src/main/kotlin/no/elhub/auth/features/documents/confirm/Route.kt
@@ -5,7 +5,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receiveChannel
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.patch
+import io.ktor.server.routing.put
 import io.ktor.utils.io.readRemaining
 import kotlinx.io.readByteArray
 import no.elhub.auth.features.common.InputError
@@ -16,22 +16,22 @@ import no.elhub.devxp.jsonapi.response.JsonApiErrorCollection
 const val DOCUMENT_ID_PARAM = "id"
 
 fun Route.route(handler: Handler) {
-    patch("/{$DOCUMENT_ID_PARAM}.pdf") {
+    put("/{$DOCUMENT_ID_PARAM}.pdf") {
         val documentId = validateId(call.parameters[DOCUMENT_ID_PARAM])
             .getOrElse { error ->
                 val (status, body) = error.toApiErrorResponse()
                 call.respond(status, JsonApiErrorCollection(listOf(body)))
-                return@patch
+                return@put
             }
 
         val signedDocument = call.receiveChannel().readRemaining().readByteArray()
         if (signedDocument.isEmpty()) {
             val (status, body) = InputError.MissingInputError.toApiErrorResponse()
             call.respond(status, JsonApiErrorCollection(listOf(body)))
-            return@patch
+            return@put
         }
 
-        val result = handler(
+        handler(
             Command(
                 documentId = documentId,
                 signedFile = signedDocument
@@ -39,14 +39,15 @@ fun Route.route(handler: Handler) {
         ).getOrElse { error ->
             when (error) {
                 ConfirmDocumentError.DocumentNotFoundError -> call.respond(HttpStatusCode.NotFound)
+
                 ConfirmDocumentError.DocumentReadError,
                 ConfirmDocumentError.DocumentUpdateError,
                 ConfirmDocumentError.ScopeReadError,
                 ConfirmDocumentError.GrantCreationError -> call.respond(HttpStatusCode.InternalServerError)
             }
-            return@patch
+            return@put
         }
 
-        call.respond(HttpStatusCode.OK, result.toResponse())
+        call.respond(HttpStatusCode.NoContent)
     }
 }
