e9d01da5124735892bd8aba2c98abfdd348c235b
Lise May Le <lise.le@statnett.no>
2025-12-03 09:03:56 +0100

    Add error handling when inserting authorization-request properties [patch] (#261)
    * Add error handling when inserting authorization-request properties [patch]
    
    * Add tests for ExposedRequestPropertiesRepository
    
    * Fix lint issues

--- src/main/kotlin/no/elhub/auth/features/requests/common/RequestPropertiesRepository.kt
+++ src/main/kotlin/no/elhub/auth/features/requests/common/RequestPropertiesRepository.kt
@@ -1,38 +1,33 @@
 package no.elhub.auth.features.requests.common
 
-import org.jetbrains.exposed.sql.ResultRow
+import arrow.core.Either
+import arrow.core.raise.either
+import no.elhub.auth.features.common.RepositoryError
+import no.elhub.auth.features.common.RepositoryWriteError
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.batchInsert
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
-import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.util.UUID
 
 interface RequestPropertiesRepository {
-    fun insert(properties: List<AuthorizationRequestProperty>)
-    fun find(requestId: UUID): List<AuthorizationRequestProperty>
+    fun insert(properties: List<AuthorizationRequestProperty>): Either<RepositoryError, Unit>
 }
 
 class ExposedRequestPropertiesRepository : RequestPropertiesRepository {
-    override fun insert(properties: List<AuthorizationRequestProperty>) {
-        if (properties.isEmpty()) return
 
-        transaction {
-            AuthorizationRequestPropertyTable.batchInsert(properties) { property ->
-                this[AuthorizationRequestPropertyTable.requestId] = property.requestId
-                this[AuthorizationRequestPropertyTable.key] = property.key
-                this[AuthorizationRequestPropertyTable.value] = property.value
+    override fun insert(properties: List<AuthorizationRequestProperty>): Either<RepositoryError, Unit> =
+        either<RepositoryWriteError, Unit> {
+            if (properties.isEmpty()) return@either
+
+            transaction {
+                AuthorizationRequestPropertyTable.batchInsert(properties) { property ->
+                    this[AuthorizationRequestPropertyTable.requestId] = property.requestId
+                    this[AuthorizationRequestPropertyTable.key] = property.key
+                    this[AuthorizationRequestPropertyTable.value] = property.value
+                }
             }
-        }
-    }
-    override fun find(requestId: UUID): List<AuthorizationRequestProperty> =
-        transaction {
-            AuthorizationRequestPropertyTable
-                .selectAll()
-                .where { AuthorizationRequestPropertyTable.requestId eq requestId }
-                .map { it.toAuthorizationRequestProperty() }
-        }
+        }.mapLeft { RepositoryWriteError.UnexpectedError }
 }
 
 object AuthorizationRequestPropertyTable : Table("authorization_request_property") {
@@ -41,9 +36,3 @@ object AuthorizationRequestPropertyTable : Table("authorization_request_property
     val value = text("value")
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
 }
-
-fun ResultRow.toAuthorizationRequestProperty() = AuthorizationRequestProperty(
-    requestId = this[AuthorizationRequestPropertyTable.requestId],
-    key = this[AuthorizationRequestPropertyTable.key],
-    value = this[AuthorizationRequestPropertyTable.value],
-)
