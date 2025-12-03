3723537b9db5ccc7cf1fd88b685c33b07dde530e
Vebjorn Fjeldberg <vebjorn.fjeldberg@statnett.no>
2025-12-03 14:24:28 +0100

    Add missing document relationships in response and clean up exposed

--- src/main/kotlin/no/elhub/auth/features/documents/common/AuthorizationDocumentProperty.kt
+++ src/main/kotlin/no/elhub/auth/features/documents/common/AuthorizationDocumentProperty.kt
@@ -1,9 +1,6 @@
 package no.elhub.auth.features.documents.common
 
-import java.util.UUID
-
 data class AuthorizationDocumentProperty(
-    val documentId: UUID,
     val key: String,
     val value: String
 )
