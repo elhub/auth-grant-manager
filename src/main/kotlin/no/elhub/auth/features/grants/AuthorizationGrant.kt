dfa90806685ebaeb68fcd2fe18cfa0fb2174b60d
Lise May Le <lise.le@statnett.no>
2025-12-10 21:17:14 +0100

    Refactor to use factory method. Also use type-safe UUID as id instead of String.

--- src/main/kotlin/no/elhub/auth/features/grants/AuthorizationGrant.kt
+++ src/main/kotlin/no/elhub/auth/features/grants/AuthorizationGrant.kt
@@ -1,11 +1,11 @@
 package no.elhub.auth.features.grants
 
-import kotlinx.datetime.LocalDateTime
 import no.elhub.auth.features.common.party.AuthorizationParty
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class AuthorizationGrant(
-    val id: String,
+    val id: UUID,
     val grantStatus: Status,
     val grantedFor: AuthorizationParty,
     val grantedBy: AuthorizationParty,
@@ -16,6 +16,28 @@ data class AuthorizationGrant(
     val sourceType: SourceType,
     val sourceId: UUID
 ) {
+    companion object {
+        fun create(
+            grantedFor: AuthorizationParty,
+            grantedBy: AuthorizationParty,
+            grantedTo: AuthorizationParty,
+            sourceType: SourceType,
+            sourceId: UUID
+        ): AuthorizationGrant =
+            AuthorizationGrant(
+                id = UUID.randomUUID(),
+                grantStatus = Status.Active,
+                grantedFor = grantedFor,
+                grantedBy = grantedBy,
+                grantedTo = grantedTo,
+                grantedAt = LocalDateTime.now(),
+                validFrom = LocalDateTime.now(),
+                validTo = LocalDateTime.now().plusYears(1), // TODO this will be handled by the value stream
+                sourceId = sourceId,
+                sourceType = sourceType,
+            )
+    }
+
     enum class Status {
         Active,
         Exhausted,
