8d7a509a70ad2c467d8ad4dc445a4e8a8224786f
Lise May Le <lise.le@statnett.no>
2025-12-15 09:33:55 +0100

    Commit changes done that were accidentally removed during rebase and force push

--- buildSrc/src/main/kotlin/no/elhub/auth/utils/JsonApiSpecValidator.kt
+++ buildSrc/src/main/kotlin/no/elhub/auth/utils/JsonApiSpecValidator.kt
@@ -1,34 +1,40 @@
 package no.elhub.auth.utils
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.networknt.schema.JsonSchemaFactory
-import com.networknt.schema.SpecVersion
+import com.networknt.schema.InputFormat
+import com.networknt.schema.Schema
+import com.networknt.schema.SchemaLocation
+import com.networknt.schema.SchemaRegistry
+import com.networknt.schema.SpecificationVersion
 import java.io.File
-import java.net.URI
 import java.nio.file.Files
 import kotlin.io.path.Path
 
 fun validateJsonApiSpec(schemasLocation: String) {
     val objectMapper = jacksonObjectMapper()
-    val factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7)
-    val schema = factory.getSchema(URI.create("https://json-schema.org/draft/2020-12/schema"))
+
+    val registry = SchemaRegistry.withDefaultDialect(SpecificationVersion.DRAFT_2020_12)
+
+    val metaSchema: Schema = registry.getSchema(
+        SchemaLocation.of("https://json-schema.org/draft/2020-12/schema")
+    )
 
     val schemasToCheck = Files.list(Path(schemasLocation))
         .filter { Files.isRegularFile(it) && it.fileName.toString().endsWith(".schema.json") }
         .map { it.fileName.toString() }
         .toList()
 
-    val outputs = schemasToCheck.map {
-        it to schema.validate(objectMapper.readTree(File("$schemasLocation/$it").inputStream()))
+    val outputs = schemasToCheck.map { fileName ->
+        val node = objectMapper.readTree(File("$schemasLocation/$fileName"))
+        fileName to metaSchema.validate(node.toString(), InputFormat.JSON)
     }
 
     val erroneousOutputs = outputs.filter { it.second.isNotEmpty() }
+
     if (erroneousOutputs.isNotEmpty()) {
         erroneousOutputs.forEach { (fileName, errors) ->
             println("👮 Found errors in $fileName:")
-            errors.forEach { error ->
-                println(" ❌ $error")
-            }
+            errors.forEach { error -> println(" ❌ $error") }
         }
     } else {
         println("🎉 All JSON API schemas are valid.")
