12ed232427a31470ff74342c87197c003bab0220
Lise May Le <lise.le@statnett.no>
2025-12-15 15:03:12 +0100

    Fix linting

--- src/test/kotlin/no/elhub/auth/features/grants/AuthorizationGrantRouteTest.kt
+++ src/test/kotlin/no/elhub/auth/features/grants/AuthorizationGrantRouteTest.kt
@@ -6,10 +6,10 @@ import io.kotest.matchers.shouldBe
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.client.request.patch
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
-import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
