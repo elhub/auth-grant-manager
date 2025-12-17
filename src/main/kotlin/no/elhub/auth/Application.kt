1084870690335d638e1bb7283563d9eaf7fe7c7b
Vebjorn Fjeldberg <vebjorn.fjeldberg@statnett.no>
2025-12-17 10:59:40 +0100

    Add plugin to validate user agent header

--- src/main/kotlin/no/elhub/auth/Application.kt
+++ src/main/kotlin/no/elhub/auth/Application.kt
@@ -3,6 +3,7 @@ package no.elhub.auth
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.netty.EngineMain
+import no.elhub.auth.config.HeaderPolicy
 import no.elhub.auth.config.configureDatabase
 import no.elhub.auth.config.configureLogging
 import no.elhub.auth.config.configureMonitoring
@@ -19,4 +20,5 @@ fun Application.module() {
     configureLogging()
     configureMonitoring(dataSource)
     configureSerialization()
+    install(HeaderPolicy)
 }
---
+++ src/main/kotlin/no/elhub/auth/config/RequestHeaderPolicy.kt
@@ -0,0 +1,31 @@
+package no.elhub.auth.config
+
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.createApplicationPlugin
+import io.ktor.server.response.respond
+import no.elhub.devxp.jsonapi.response.JsonApiErrorCollection
+import no.elhub.devxp.jsonapi.response.JsonApiErrorObject
+
+val HeaderPolicy = createApplicationPlugin(
+    name = "HeaderPolicy",
+) {
+    onCall { call ->
+        val userAgent = call.request.headers[HttpHeaders.UserAgent]
+        if (userAgent.isNullOrBlank()) {
+            call.respond(
+                HttpStatusCode.BadRequest,
+                JsonApiErrorCollection(
+                    listOf(MissingUserAgentHeder)
+                )
+            )
+            return@onCall
+        }
+    }
+}
+
+val MissingUserAgentHeder: JsonApiErrorObject =
+    JsonApiErrorObject(
+        status = "400",
+        detail = "Missing User-Agent header"
+    )
