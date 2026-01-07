3a526bb65fe2d6bec5c6c2568e8f4e112da494a6
Vebjorn Fjeldberg <vebjorn.fjeldberg@statnett.no>
2026-01-07 14:32:49 +0100

    Ensure valid json api response for get document list

--- src/main/kotlin/no/elhub/auth/features/documents/query/Response.kt
+++ src/main/kotlin/no/elhub/auth/features/documents/query/Response.kt
@@ -1,9 +1,16 @@
 package no.elhub.auth.features.documents.query
 
 import no.elhub.auth.features.documents.AuthorizationDocument
-import no.elhub.auth.features.documents.get.GetDocumentResponse
+import no.elhub.auth.features.documents.DOCUMENTS_PATH
+import no.elhub.auth.features.documents.get.GetDocumentResponseAttributes
+import no.elhub.auth.features.documents.get.GetDocumentResponseRelationships
 import no.elhub.auth.features.documents.get.toGetResponse
+import no.elhub.devxp.jsonapi.model.JsonApiLinks
+import no.elhub.devxp.jsonapi.response.JsonApiResponse
 
-typealias AuthorizationDocumentsResponse = List<GetDocumentResponse>
+typealias GetDocumentListResponse = JsonApiResponse.CollectionDocumentWithRelationships<GetDocumentResponseAttributes, GetDocumentResponseRelationships>
 
-fun List<AuthorizationDocument>.toGetResponse(): AuthorizationDocumentsResponse = this.map { it.toGetResponse() }
+fun List<AuthorizationDocument>.toGetResponse(): GetDocumentListResponse = GetDocumentListResponse(
+    data = this.map { it.toGetResponse().data },
+    links = JsonApiLinks.ResourceObjectLink(DOCUMENTS_PATH)
+)
