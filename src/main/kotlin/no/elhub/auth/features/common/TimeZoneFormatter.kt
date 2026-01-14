9a4c3ac1ca8c9d7bef6d3837452b293fd290d391
Lise May Le <lise.le@statnett.no>
2026-01-14 12:54:56 +0100

    Remove fractional seconds from timestamps [patch] (#354)
    * Remove fractional seconds from timestamps [patch]
    
    * Revert schema changes in authorization files
    
    * Remove schema file changes from PR

--- src/main/kotlin/no/elhub/auth/features/common/TimeZoneFormatter.kt
+++ src/main/kotlin/no/elhub/auth/features/common/TimeZoneFormatter.kt
@@ -3,6 +3,7 @@ package no.elhub.auth.features.common
 import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 private val TIME_ZONE = ZoneId.of("Europe/Oslo")
 private val ISO_OFFSET_FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME
@@ -10,6 +11,7 @@ private val ISO_OFFSET_FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME
 fun OffsetDateTime.toTimeZoneOffsetString(): String =
     this.toInstant()
         .atZone(TIME_ZONE)
+        .truncatedTo(ChronoUnit.SECONDS)
         .format(ISO_OFFSET_FORMATTER)
 
 fun currentTimeWithTimeZone(): OffsetDateTime = OffsetDateTime.now(TIME_ZONE)
