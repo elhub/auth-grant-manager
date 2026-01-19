f29d795fb0bf8ee23f4b708cbe879d1c29850690
Tollef Christiansen <tollef.christiansen@statnett.no>
2026-01-19 16:51:05 +0100

    tx refactor for documents feature

--- src/main/kotlin/no/elhub/auth/features/documents/confirm/Handler.kt
+++ src/main/kotlin/no/elhub/auth/features/documents/confirm/Handler.kt
@@ -1,6 +1,7 @@
 package no.elhub.auth.features.documents.confirm
 
 import arrow.core.Either
+import arrow.core.left
 import arrow.core.raise.either
 import arrow.core.raise.ensure
 import no.elhub.auth.features.common.RepositoryReadError
@@ -9,6 +10,7 @@ import no.elhub.auth.features.common.party.PartyService
 import no.elhub.auth.features.documents.AuthorizationDocument
 import no.elhub.auth.features.documents.common.DocumentRepository
 import no.elhub.auth.features.grants.AuthorizationGrant
+import org.jetbrains.exposed.sql.transactions.transaction
 import no.elhub.auth.features.grants.common.GrantRepository
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
@@ -19,13 +21,15 @@ class Handler(
     private val grantRepository: GrantRepository,
 ) {
     suspend operator fun invoke(command: Command): Either<ConfirmDocumentError, Unit> = either {
-        val document = documentRepository.find(command.documentId)
-            .mapLeft { error ->
-                when (error) {
-                    is RepositoryReadError.NotFoundError -> ConfirmDocumentError.DocumentNotFoundError
-                    is RepositoryReadError.UnexpectedError -> ConfirmDocumentError.DocumentReadError
-                }
-            }.bind()
+        val document = transaction {
+            documentRepository.find(command.documentId)
+                .mapLeft { error ->
+                    when (error) {
+                        is RepositoryReadError.NotFoundError -> ConfirmDocumentError.DocumentNotFoundError
+                        is RepositoryReadError.UnexpectedError -> ConfirmDocumentError.DocumentReadError
+                    }
+                }.bind()
+        }
 
         val requestedBy = partyService.resolve(command.requestedByIdentifier)
             .mapLeft { ConfirmDocumentError.RequestedByResolutionError }
@@ -46,40 +50,42 @@ class Handler(
         // TODO: Implement validation of the signed file and find the signatory
         val signatory = document.requestedTo
 
-        val confirmedDocument = documentRepository.confirm(
-            documentId = document.id,
-            signedFile = command.signedFile,
-            requestedFrom = document.requestedFrom,
-            signatory = document.requestedTo
-        )
-            .mapLeft { error ->
-                when (error) {
-                    is RepositoryWriteError.NotFoundError -> ConfirmDocumentError.DocumentNotFoundError
-
-                    is RepositoryWriteError.ConflictError,
-                    is RepositoryWriteError.UnexpectedError -> ConfirmDocumentError.DocumentUpdateError
-                }
-            }.bind()
-
-        val scopeIds = documentRepository.findScopeIds(confirmedDocument.id)
-            .mapLeft { error ->
-                when (error) {
-                    is RepositoryReadError.NotFoundError -> ConfirmDocumentError.DocumentNotFoundError
-                    is RepositoryReadError.UnexpectedError -> ConfirmDocumentError.ScopeReadError
-                }
-            }.bind()
-
-        val grantToCreate =
-            AuthorizationGrant.create(
-                grantedFor = confirmedDocument.requestedFrom,
-                grantedBy = signatory,
-                grantedTo = confirmedDocument.requestedBy,
-                sourceType = AuthorizationGrant.SourceType.Document,
-                sourceId = confirmedDocument.id
+        transaction {
+            val confirmedDocument = documentRepository.confirm(
+                documentId = document.id,
+                signedFile = command.signedFile,
+                requestedFrom = document.requestedFrom,
+                signatory = document.requestedTo
             )
+                .mapLeft { error ->
+                    when (error) {
+                        is RepositoryWriteError.NotFoundError -> ConfirmDocumentError.DocumentNotFoundError
 
-        grantRepository.insert(grantToCreate, scopeIds)
-            .mapLeft { ConfirmDocumentError.GrantCreationError }.bind()
+                        is RepositoryWriteError.ConflictError,
+                        is RepositoryWriteError.UnexpectedError -> ConfirmDocumentError.DocumentUpdateError
+                    }
+                }.bind()
+
+            val scopeIds = documentRepository.findScopeIds(confirmedDocument.id)
+                .mapLeft { error ->
+                    when (error) {
+                        is RepositoryReadError.NotFoundError -> ConfirmDocumentError.DocumentNotFoundError
+                        is RepositoryReadError.UnexpectedError -> ConfirmDocumentError.ScopeReadError
+                    }
+                }.bind()
+
+            val grantToCreate =
+                AuthorizationGrant.create(
+                    grantedFor = confirmedDocument.requestedFrom,
+                    grantedBy = signatory,
+                    grantedTo = confirmedDocument.requestedBy,
+                    sourceType = AuthorizationGrant.SourceType.Document,
+                    sourceId = confirmedDocument.id
+                )
+
+            grantRepository.insert(grantToCreate, scopeIds)
+                .mapLeft { ConfirmDocumentError.GrantCreationError }.bind()
+        }
     }
 }
 
--- src/main/kotlin/no/elhub/auth/features/documents/get/Handler.kt
+++ src/main/kotlin/no/elhub/auth/features/documents/get/Handler.kt
@@ -3,11 +3,13 @@ package no.elhub.auth.features.documents.get
 import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensure
+import arrow.core.left
 import no.elhub.auth.features.common.QueryError
 import no.elhub.auth.features.common.RepositoryReadError
 import no.elhub.auth.features.documents.AuthorizationDocument
 import no.elhub.auth.features.documents.common.DocumentRepository
 import no.elhub.auth.features.grants.AuthorizationGrant
+import org.jetbrains.exposed.sql.transactions.transaction
 import no.elhub.auth.features.grants.common.GrantRepository
 
 class Handler(
@@ -15,28 +17,32 @@ class Handler(
     private val grantRepository: GrantRepository
 ) {
     operator fun invoke(query: Query): Either<QueryError, AuthorizationDocument> = either {
-        val document = documentRepo.find(query.documentId)
-            .mapLeft { error ->
-                when (error) {
-                    is RepositoryReadError.NotFoundError -> QueryError.ResourceNotFoundError
-                    is RepositoryReadError.UnexpectedError -> QueryError.IOError
-                }
-            }.bind()
+        val document = transaction {
+            documentRepo.find(query.documentId)
+                .mapLeft { error ->
+                    when (error) {
+                        is RepositoryReadError.NotFoundError -> QueryError.ResourceNotFoundError
+                        is RepositoryReadError.UnexpectedError -> QueryError.IOError
+                    }
+                }.bind()
+        }
 
         ensure(
             query.authorizedParty == document.requestedBy ||
-                query.authorizedParty == document.requestedFrom
+                    query.authorizedParty == document.requestedFrom
         ) {
             QueryError.NotAuthorizedError
         }
 
-        val grant = grantRepository.findBySource(AuthorizationGrant.SourceType.Document, document.id)
-            .mapLeft { error ->
-                when (error) {
-                    RepositoryReadError.NotFoundError -> QueryError.ResourceNotFoundError
-                    RepositoryReadError.UnexpectedError -> QueryError.IOError
-                }
-            }.bind()
+        val grant = transaction {
+            grantRepository.findBySource(AuthorizationGrant.SourceType.Document, document.id)
+                .mapLeft { error ->
+                    when (error) {
+                        RepositoryReadError.NotFoundError -> QueryError.ResourceNotFoundError
+                        RepositoryReadError.UnexpectedError -> QueryError.IOError
+                    }
+                }.bind()
+        }
 
         document.copy(
             grantId = grant?.id,
