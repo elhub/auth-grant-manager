cfec875300f51129be5f4aca393b1b29266c21c7
Tollef Christiansen <tollef.christiansen@statnett.no>
2026-02-10 15:48:02 +0100

    unused reference

--- src/test/kotlin/no/elhub/auth/features/filegenerator/PdfGeneratorTest.kt
+++ src/test/kotlin/no/elhub/auth/features/filegenerator/PdfGeneratorTest.kt
@@ -1,41 +1,16 @@
 package no.elhub.auth.features.filegenerator
 
-import arrow.core.getOrElse
-import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
-import io.kotest.assertions.fail
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.collections.shouldContain
-import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.collections.shouldNotContain
 import io.kotest.matchers.nulls.shouldBeNull
-import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldNotContain
 import kotlinx.datetime.LocalDate
 import no.elhub.auth.features.businessprocesses.changeofsupplier.domain.ChangeOfSupplierBusinessMeta
 import no.elhub.auth.features.businessprocesses.movein.domain.MoveInBusinessMeta
-import no.elhub.auth.features.common.CreateScopeData
-import no.elhub.auth.features.common.PostgresTestContainer
-import no.elhub.auth.features.common.PostgresTestContainerExtension
-import no.elhub.auth.features.common.currentTimeWithTimeZone
-import no.elhub.auth.features.common.party.AuthorizationParty
-import no.elhub.auth.features.common.party.ExposedPartyRepository
-import no.elhub.auth.features.common.party.PartyType
-import no.elhub.auth.features.documents.AuthorizationDocument
-import no.elhub.auth.features.filegenerator.PdfGenerator
-import no.elhub.auth.features.filegenerator.PdfGeneratorConfig
-import no.elhub.auth.features.grants.AuthorizationScope
-import no.elhub.auth.features.grants.common.AuthorizationScopeTable
 import org.apache.pdfbox.Loader
 import org.apache.pdfbox.text.PDFTextStripper
-import org.jetbrains.exposed.v1.core.eq
-import org.jetbrains.exposed.v1.jdbc.Database
-import org.jetbrains.exposed.v1.jdbc.selectAll
-import org.jetbrains.exposed.v1.jdbc.transactions.transaction
-import java.util.UUID
 
 class PdfGeneratorTest :
     FunSpec({
