81c06572baa92a5177ab2f1a3b1b4b53f1d64152
Sander Sandøy <sander.sandoy@fyk.no>
2026-02-17 14:01:36 +0100

    chore: lint:fix

--- src/main/kotlin/no/elhub/auth/features/documents/create/dto/JsonApiCreateRequest.kt
+++ src/main/kotlin/no/elhub/auth/features/documents/create/dto/JsonApiCreateRequest.kt
@@ -1,13 +1,11 @@
 package no.elhub.auth.features.documents.create.dto
 
 import kotlinx.datetime.LocalDate
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import no.elhub.auth.features.common.party.AuthorizationParty
 import no.elhub.auth.features.common.party.PartyIdentifier
 import no.elhub.auth.features.documents.AuthorizationDocument
 import no.elhub.auth.features.documents.create.model.CreateDocumentModel
-import no.elhub.auth.features.filegenerator.SupportedLanguage
 import no.elhub.devxp.jsonapi.model.JsonApiAttributes
 import no.elhub.devxp.jsonapi.model.JsonApiResourceMeta
 import no.elhub.devxp.jsonapi.request.JsonApiRequest
