aeaacc284a433fd2f8c3dd8db75f995daf60f7dd
Lise May Le <lise.le@statnett.no>
2026-02-20 15:33:37 +0100

    Remove /authorization-grants/{id}/scopes/{id} from openapi spec [patch]

---
+++ src/main/kotlin/no/elhub/auth/features/common/Module.kt
@@ -0,0 +1,23 @@
+package no.elhub.auth.features.common
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.response.respond
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
+import no.elhub.devxp.jsonapi.response.JsonApiErrorCollection
+
+fun Application.module() {
+    routing {
+        route("{...}") {
+            handle {
+                val (status, body) = buildApiErrorResponse(
+                    HttpStatusCode.NotFound,
+                    "Endpoint not found",
+                    "The requested endpoint does not exist. Please check the URL and HTTP method."
+                )
+                call.respond<JsonApiErrorCollection>(status, body)
+            }
+        }
+    }
+}
