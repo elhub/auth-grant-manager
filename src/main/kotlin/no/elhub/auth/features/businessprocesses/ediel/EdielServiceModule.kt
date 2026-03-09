a33d8a1bf9689b7e9d0bf4a9a7962256802d2aa9
Sander Sandøy <sander.sandoy@fyk.no>
2026-03-09 11:11:24 +0100

    feat: add httpProxyClient which EdielService can use to call Ediel (#518)

--- src/main/kotlin/no/elhub/auth/features/businessprocesses/ediel/EdielServiceModule.kt
+++ src/main/kotlin/no/elhub/auth/features/businessprocesses/ediel/EdielServiceModule.kt
@@ -25,7 +25,7 @@ fun Application.edielServiceModule() {
         single<EdielService> {
             EdielApi(
                 edielApiConfig = get(),
-                client = get(named("commonHttpClient"))
+                client = get(named("proxyHttpClient"))
             )
         }
     }
