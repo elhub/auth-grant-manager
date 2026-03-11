8368c2c32694df65ab203b1c0775e2a2aca09e9c
Tollef Christiansen <tollef.christiansen@statnett.no>
2026-03-11 14:32:19 +0100

    add partytype.system check to grants consume handler

--- src/main/kotlin/no/elhub/auth/features/grants/consume/Handler.kt
+++ src/main/kotlin/no/elhub/auth/features/grants/consume/Handler.kt
@@ -4,6 +4,7 @@ import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensure
 import no.elhub.auth.features.common.currentTimeUtc
+import no.elhub.auth.features.common.party.PartyType
 import no.elhub.auth.features.grants.AuthorizationGrant
 import no.elhub.auth.features.grants.AuthorizationGrant.Status
 import no.elhub.auth.features.grants.common.GrantRepository
@@ -13,6 +14,9 @@ class Handler(
     private val repo: GrantRepository
 ) {
     operator fun invoke(command: ConsumeCommand): Either<ConsumeError, AuthorizationGrant> = either {
+        ensure(command.authorizedParty.type == PartyType.System) {
+            ConsumeError.NotAuthorized
+        }
         ensure(command.newStatus == Status.Exhausted) {
             ConsumeError.IllegalTransitionError
         }
