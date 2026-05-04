01d1db6cbe5e1029d4e86128e1f279d2b8bec6de
Tollef Christiansen <tollef.christiansen@statnett.no>
2026-05-04 12:27:24 +0200

    dedupe enum request param

--- src/main/kotlin/no/elhub/auth/features/common/EnumValidator.kt
+++ src/main/kotlin/no/elhub/auth/features/common/EnumValidator.kt
@@ -2,6 +2,7 @@ package no.elhub.auth.features.common
 
 import arrow.core.Either
 import arrow.core.right
+import kotlin.collections.distinct
 
 // Returns a list of enum values from a comma-separated string,
 // or left if string has incorrect format
@@ -13,7 +14,7 @@ inline fun <reified T : Enum<T>> validateEnumListParam(
         if (param.isNullOrBlank()) {
             return emptyList<T>().right()
         }
-        param.split(',').map { enumValueOf<T>(it) }
+        param.split(',').map { enumValueOf<T>(it) }.distinct()
     }.mapLeft {
         InputError.MalformedInputError(
             "Invalid $paramName value '$param'. Valid values: ${enumValues<T>().joinToString()}"
