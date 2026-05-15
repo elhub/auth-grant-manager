00969261f93a5ef8a12d30461881c318d6501fdb
Vebjorn Fjeldberg <vebjorn.fjeldberg@statnett.no>
2026-05-15 13:39:20 +0200

    Enforce ElhubTraceID to be UUID format

--- src/main/kotlin/no/elhub/auth/config/ErrorHandling.kt
+++ src/main/kotlin/no/elhub/auth/config/ErrorHandling.kt
@@ -1,18 +1,30 @@
 package no.elhub.auth.config
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respond
 import kotlinx.serialization.ExperimentalSerializationApi
+import no.elhub.auth.features.common.buildApiErrorResponse
 import no.elhub.auth.features.common.toInternalServerApiErrorResponse
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("ErrorHandling")
 
+class InvalidTraceIdException : Exception()
+
 @OptIn(ExperimentalSerializationApi::class)
 fun Application.configureErrorHandling() {
     install(StatusPages) {
+        exception<InvalidTraceIdException> { call, cause ->
+            val (status, body) = buildApiErrorResponse(
+                status = HttpStatusCode.BadRequest,
+                title = "Invalid trace ID",
+                detail = "Header 'ElhubTraceID' must be a valid UUID"
+            )
+            call.respond(status, body)
+        }
         exception<Throwable> { call, cause ->
             val root = generateSequence(cause) { it.cause }.last()
             logger.error("Unhandled exception", cause)
